1905bb077881a86e141fe05878742ffdff66b588
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-27 02:05:03 +0545

    Create karma.py

---
+++ Infamous/karma.py
@@ -0,0 +1,99 @@
+# https://github.com/dilshadhasnain07/YaeMiko
+# https://github.com/Team-ProjectCodeX
+# https://t.me/hasnainkk
+
+# <============================================== IMPORTS =========================================================>
+from pyrogram.types import InlineKeyboardButton as ib
+from telegram import InlineKeyboardButton
+
+from MukeshRobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
+
+# <============================================== CONSTANTS =========================================================>
+START_IMG = [
+    "https://telegra.ph/file/40b93b46642124605e678.jpg",
+    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
+    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
+    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
+    "https://telegra.ph/file/262c86393730a609cdade.jpg",
+    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+]
+
+HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
+
+ALIVE_ANIMATION = [
+    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
+    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
+    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
+    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+]
+
+FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
+
+PM_START_TEXT = """ 
+✨ *ɪ ᴀᴍ ᴍɪᴋᴏ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ.*
+────────────────
+  
+─────────────────
+"""
+
+START_BTN = [
+    [
+        InlineKeyboardButton(
+            text="⛩ ADD ME TO YOUR GROUP ⛩",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="HELP", callback_data="help_back"),
+    ],
+    [
+        InlineKeyboardButton(text="DETAILS", callback_data="Miko_"),
+        InlineKeyboardButton(text="AI", callback_data="ai_handler"),
+        InlineKeyboardButton(text="SOURCE", callback_data="git_source"),
+        InlineKeyboardButton(text="MUSIC", callback_data="Music_"),
+    ],
+    [
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
+    ],
+]
+
+GROUP_START_BTN = [
+    [
+        InlineKeyboardButton(
+            text="⛩ ADD ME TO YOUR GROUP ⛩",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
+        InlineKeyboardButton(text="NETWORK", url=f"https://t.me/SHINOBI_NETWORK"),
+    ], 
+]
+
+ALIVE_BTN = [
+    [
+        ib(text="𝚄𝙿𝙳𝙰𝚃𝙴", url="https://t.me/ANIME_NETWORK07"),
+        ib(text="𝚂𝚄𝙿𝙿𝙾𝚁𝚃", url="https://t.me/SHINOBI_HEADQUATERS"),
+    
+    ],
+    [
+        ib(
+            text="𝙰𝙳𝙳 𝙼𝙴 𝙸𝙽 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 💫",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+]
+
+HELP_STRINGS = """
+🫧 *Yae-Miko* 🫧
+
+☉ *Here, you will find a list of all the available commands.*
+
+ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /
+"""
