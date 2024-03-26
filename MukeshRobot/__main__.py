e7c02ff4e2c3b3e8ffc0ef63dd3851023a4d2c04
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-27 04:46:11 +0545

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -91,7 +91,7 @@ buttons = [
         InlineKeyboardButton(text="SOURCE", callback_data="source_"),
         InlineKeyboardButton(text="DETAIL", callback_data="mukesh_"),
     ],
-        InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/SHINOBI_HEADQUATERS"),
+        InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
         InlineKeyboardButton(text="HELP", callback_data="Main_help"),
         InlineKeyboardButton(text="ADVANCED", callback_data="Basic_help"),
     ],
