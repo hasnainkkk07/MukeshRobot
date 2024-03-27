52a3926ea9f4bcc362732994dde69fc7e4c42b85
dilshadhasnain13 <162882004+dilshadhasnain13@users.noreply.github.com>
2024-03-27 05:51:14 +0530

    Create whispers.py

---
+++ MukeshRobot/modules/no_sql/whispers.py
@@ -0,0 +1,19 @@
+from MukeshRobot.modules.no_sql import dbname
+from MukeshRobot import MONGO_DB_URI, dispatcher
+collection = dbname["whisper"]
+
+
+class Whispers:
+    @staticmethod
+    async def add_whisper(WhisperId, WhisperData):
+        whisper = {"WhisperId": WhisperId, "whisperData": WhisperData}
+        await collection.insert_one(whisper)
+
+    @staticmethod
+    async def del_whisper(WhisperId):
+        await collection.delete_one({"WhisperId": WhisperId})
+
+    @staticmethod
+    async def get_whisper(WhisperId):
+        whisper = await collection.find_one({"WhisperId": WhisperId})
+        return whisper["whisperData"] if whisper else None
